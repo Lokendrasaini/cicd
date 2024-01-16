8f39fa19e042284f967d423195a09707f12a964a
Lokendra <lokendrasaini1995@gmail.com>
2024-01-16 13:04:38 +0530

    ww

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! Declarative loki !!</p>'; ?> 
+ <?php echo '<p>Hello world from PHP! loki !!</p>'; ?> 
  </body>
 </html>
